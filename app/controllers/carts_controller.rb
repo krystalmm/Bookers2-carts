c7983cf5f57f73ebb34c11897406aafd250257d4
krystalmm <taeyeon_aina@live.jp>
2021-03-15 15:50:25 +0900

    [add] address

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,11 +1,10 @@
 class CartsController < ApplicationController
   def index
     @carts = Cart.where(user_id: current_user.id)
-    
   end
 
   def create
-    @cart_book = current_user.carts.build(cart_books_params)
+    @cart_book = current_user.carts.create(cart_books_params)
     @cart_book.save
     redirect_to carts_path
   end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,21 +1,29 @@
 class OrdersController < ApplicationController
+  before_action :set_cart, only: [:create]
+
   def new
     @cart = Cart.where(user_id: current_user.id)
-    if @cart.books.empty?
-      redirect_to books_path, notice: 'カートは空です'
-      return
-    end
     @order = Order.new
   end
 
   def confirm
     @order = Order.new(order_params)
     @carts = Cart.where(user_id: current_user.id)
+    if params[:order][:address_option] == "0"
+      @order.postcode = current_user.postcode
+    elsif params[:order][:address_option] == "1"
+      @order.postcode = current_user.postcode
+    elsif params[:order][:address_option] == "2"
+      @order.postcode = params[:order][:postcode]
+      @order.address = params[:order][:address]
+      @order.address_name = params[:order][:address_name]
+    end
   end
 
   def create
     @order = Order.new(order_params)
     if @order.save
+      @cart.destroy_all
       redirect_to success_orders_path
     else
       render :new
@@ -37,5 +45,9 @@ class OrdersController < ApplicationController
 
   def order_params
     params.require(:order).permit(:payment_selection, :postcode, :address, :address_name, :user_id, :postage)
+  end
+
+  def set_cart
+    @cart = Cart.where(user_id: current_user.id)
   end
 end
